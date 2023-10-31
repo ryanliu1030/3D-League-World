76aae21d2a521fbff52b7a286be4579239269e15
JacobGuiang <jacob.guiang1@gmail.com>
2023-10-31 04:35:56 -0500

    Add Nunu with idle animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { Canvas } from '@react-three/fiber';
 import { PerspectiveCamera, OrbitControls } from '@react-three/drei';
+import { Suspense } from 'react';
 import Floor from './components/Floor';
 import Cube from './components/Cube';
+import Nunu from './components/Nunu';
 import './styles.css';
 
 function App() {
@@ -11,6 +13,10 @@ function App() {
         <PerspectiveCamera position={[2, 2, 2]} makeDefault />
         <OrbitControls />
         <ambientLight />
+        <directionalLight position={[2, 2, 2]} />
+        <Suspense fallback={null}>
+          <Nunu scale={[0.005, 0.005, 0.005]} />
+        </Suspense>
         <Floor />
         <Cube position={[1, 0.05, 0]} />
         <Cube position={[0, 0.05, 1]} />
