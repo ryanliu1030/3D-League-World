3997b168f9309b21008c1faa0a370dd3413bdfee
liuxinrui5257 <xxl180033@utdallas.edu>
2023-11-26 19:38:21 -0600

    en v color modified

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import FolioCanvas from "./folio/src/javascript/FolioCanvas";
+import "./folio/src/style/main.css";
+
+function App() {
+  return (
+    <div className="w-screen h-screen">
+      <FolioCanvas />
+    </div>
+  );
+}
+
+export default App;
